4d66e8930892ed586219a682daa77685325dd2f7
Dr3adz <daniel.bornstrand@gloot.com>
2017-10-26 17:24:53 +0200

    work on ingredients selector

--- src/modules/Ingredient/components/IngredientAndAmountSelect/IngredientAndAmountSelect.js
+++ src/modules/Ingredient/components/IngredientAndAmountSelect/IngredientAndAmountSelect.js
@@ -2,48 +2,64 @@ import React from 'react';
 import Select from 'react-select';
 
 const presetMeasures = [
-  { value: 'g', label: 'grams'},
-  { value: 'cup', label: 'cups'},
+  { value: 'g', label: 'grams' },
+  { value: 'cup', label: 'cups' },
 ];
 
 export default class IngredientSelect extends React.Component {
   constructor(props) {
     super(props);
     this.initialState = {
-      tempValue: {
-        value: 'custom',
+      selectedIngredient: {
+        value: '',
         label: '',
       },
+      custom: true,
+      unit: 'g',
+      amount: 0,
+      customAmount: '',
       report: {},
     };
     this.state = this.initialState;
   }
 
-  unselectIngredient = (category, input) => {
-    input.onChange(input.value.filter(v => v !== category));
+  unselectIngredient = (entry, input) => {
+    input.onChange(input.value.filter(e => e.ingredient.value !== entry.ingredient.value));
   }
 
   addIngredient = ingredient => {
-    this.props.input.onChange([this.state.tempValue]);
+    const value = {
+      ingredient: this.state.selectedIngredient,
+      amount: this.state.amount,
+      unit: this.state.unit,
+      custom: this.state.custom,
+    };
+    this.props.input.onChange([...this.props.input.value, value]);
     this.setState(this.initialState);
   }
 
   valueSelected = option => {
-    if (option.value && option.value !== 'custom') {
+    if (option.value) {
       this.props.getReport(option.value).then(report => {
         this.setState({ report });
         console.log(report);
       });
     }
-    this.setState({ tempValue: option });
+    this.setState({ selectedIngredient: option, custom: false });
   }
+
+  handleUnitChange = unit => this.setState({ unit });
+  handleAmountChange = event => this.setState({ amount: event.target.value });
+
+  isEntryValid = () => this.state.unit && this.state.amount && this.state.selectedIngredient;
 
   onType = val => {
     this.setState({
-      tempValue: {
-        value: 'custom',
+      selectedIngredient: {
+        value: '',
         label: val,
-      }
+      },
+      custom: true,
     });
     return this.props.getIngredients(val);
   }
@@ -54,40 +70,42 @@ export default class IngredientSelect extends React.Component {
       <div className="field">
         <label className="label">{label}</label>
         <div className="tags">
-          {input.value.map((ingredient, index) => (
-            <span key={index} className="tag is-info is-medium">
-              {ingredient.label}
-              <button onClick={e => this.unselectIngredient(category, input)} className="delete is-small"></button>
-            </span>))
+          {input.value.map((entry, index) => (
+            <div key={index} class="tags has-addons">
+              <span class="tag is-dark">{entry.ingredient.label}</span>
+              <span class="tag is-info">{entry.amount}{entry.unit}</span>
+              <a class="tag is-delete"></a>
+              <button onClick={e => this.unselectIngredient(entry, input)} className="delete is-small"></button>
+            </div>))
           }
         </div>
         <div className="flex">
           <Select.Async
             name="Ingredient"
             className="grow"
-            value={this.state.tempValue}
+            value={this.state.selectedIngredient}
             loadOptions={this.onType}
             onChange={this.valueSelected}
           />
-          {this.state.tempValue.value !== 'custom' &&
+          {!this.state.custom &&
             <div className="flex grow">
-              <input placeholder="amount" type="number" step="0.2" className="input grow" style={({ width: 'auto' })} />
+              <input placeholder="amount" onChange={this.handleAmountChange} value={this.state.amount} type="number" step="0.2" className="input grow" style={({ width: 'auto' })} />
               <Select
                 name="amount"
-                value="g"
+                value={this.state.unit}
                 className="grow"
                 clearable={false}
                 options={presetMeasures}
-                onChange={console.log}
+                onChange={this.handleUnitChange}
               />
             </div>
           }
-          {this.state.tempValue.value === 'custom' && this.state.tempValue.label &&
-          <div>
-            <input className="input grow" placeholder="2 cloves" />
-          </div>
+          {this.state.custom && this.state.selectedIngredient.label &&
+            <div>
+              <input className="input grow" placeholder="2 cloves" />
+            </div>
           }
-          <button onClick={this.addIngredient} className="button is-primary">add</button>
+          <button disabled={!this.isEntryValid()} onClick={this.addIngredient} className="button is-primary">add</button>
         </div>
       </div>
     );
